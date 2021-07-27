4cbb450f59cb2a8894de3e8ac71b13ac7ae64c71
hanopcan <hannah@opcan.co.uk>
2021-07-27 09:53:41 +0100

    Add cabin analytics

--- inc/enqueue.php
+++ inc/enqueue.php
@@ -6,6 +6,7 @@
  */
 
 add_action( 'wp_enqueue_scripts', 'neve_child_load_css', 20 );
+add_action( 'wp_enqueue_scripts', 'tgwf_load_footer_scripts' );
 
 
 if ( ! function_exists( 'neve_child_load_css' ) ) :
@@ -17,3 +18,10 @@ if ( ! function_exists( 'neve_child_load_css' ) ) :
 		wp_enqueue_style( 'neve-child-style', trailingslashit( get_stylesheet_directory_uri() ) . '/assets/css/main.min.css', array( 'neve-style' ), $css_version );
 	}
 endif;
+
+/**
+ * Load scripts into the footer.
+ */
+function tgwf_load_footer_scripts() {
+	wp_enqueue_script( 'cabin', 'https://scripts.withcabin.com/hello.js', array(), '1.0.0', true );
+}
