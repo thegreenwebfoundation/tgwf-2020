eda3cd6b6ee2546e60c0db9a06a8ace49f7c9c34
hanopcan <hannah@opcan.co.uk>
2021-10-25 14:05:35 +0100

    Style news categories and category description output

--- inc/news-archive.php
+++ inc/news-archive.php
@@ -9,18 +9,28 @@ add_action( 'neve_before_posts_loop', 'tgwf_display_news_categories' );
 
 function tgwf_display_news_categories() {
 
-	$categories = get_categories(
-		array(
-			'orderby' => 'name',
-			'parent'  => 0,
-		)
-	);
+	if ( is_home() ) :
 
-	foreach ( $categories as $category ) {
-		printf(
-			'<a class="wp-block-button__link" href="%1$s">%2$s</a>',
-			esc_url( get_category_link( $category->term_id ) ),
-			esc_html( $category->name )
+		$categories = get_categories(
+			array(
+				'orderby' => 'name',
+				'parent'  => 0,
+			)
 		);
-	}
+
+		echo '<div class="blog-categories-list summary-highlight">';
+		echo '<h3>Browse our posts by topic</h3>';
+
+		foreach ( $categories as $category ) {
+			echo '<div class="wp-block-button" style="display: inline-block;">';
+			printf(
+				'<a class="wp-block-button__link" href="%1$s">%2$s</a>',
+				esc_url( get_category_link( $category->term_id ) ),
+				esc_html( $category->name )
+			);
+			echo '</div>';
+		}
+
+		echo '</div>';
+	endif;
 }
