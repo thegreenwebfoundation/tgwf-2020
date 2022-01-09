317f9695dd421db492ad33baee3b828f520e98ff
Chris Adams <chris@productscience.co.uk>
2022-01-09 15:30:57 +0100

    Remove all CSS grid cleverness and libraries, back to minimal js

--- assets/js/fog-of-enactment.js
+++ assets/js/fog-of-enactment.js
@@ -1,4 +1,19 @@
 
+/**
+ * Accept an element, and if it's a link inside suumary element,
+ * open the parent details element to show extra content
+ * @param  {HTMLElement} elem
+ */
+function openDetailsIfSummary(elem) {
+  if (elem.tagName == "A" && elem.parentNode.tagName == "SUMMARY") {
+
+    // we have a link, check if we're in a details element
+    const detailsElem = elem.parentNode.parentNode
+
+    // toggle the open/closed state
+    detailsElem.open = !detailsElem.open
+  }
+}
 
 
 /**
@@ -6,7 +21,7 @@
  * default jumping behaviour to use the browser's scrollIntoView
  * feature
  *
- * @param  elem - a DOM element, typically in a list from querySelector
+ * @param  {HTMLElement} elem - a DOM element, typically in a list from querySelector
  */
 function addScrollBehaviourToLink(elem) {
   //
@@ -17,27 +32,26 @@ function addScrollBehaviourToLink(elem) {
       return
     }
 
-    console.log(elem)
-    window.elem = elem
-
-    // does this heading have further items to disclose?
-    if (elem.tagName == "A" && elem.parentNode.tagName == "SUMMARY") {
-
-      // we have a link, check if we're in a details element
-      const detailsElem = elem.parentNode.parentNode
-
-      // toggle the open/closed state
-      detailsElem.open = !detailsElem.open
-
-
-    }
     // cancel the default behaviour to avoid the confusing jump to the page
     event.stopPropagation();
     event.preventDefault();
 
+    // does this heading have further items to disclose?
+    // Trigger open / close if necessary
+    openDetailsIfSummary(elem)
+
     // then scroll into view
     if (elem.hash) {
-      document.querySelector(elem.hash).scrollIntoView({ behavior: "smooth" })
+      document.querySelector(elem.hash).scrollIntoView(
+        {
+          behavior: "smooth",
+          block: "center"
+        }
+      )
+      // finally update the location bar with the correct url fragment
+      // this allows us to come back to it in history or share
+      // links to a given section
+      history.pushState({}, "", elem.hash)
     }
   })
 }
@@ -49,28 +63,26 @@ function addScrollBehaviourToLink(elem) {
 function listAllHeadingsWithIds() {
   const linkedHeadings = document.querySelectorAll('article h2, article h3')
 
-  console.log("Listing h2 and h3  for nav headings")
+  console.log("Listing h2 and h3 for nav headings")
 
   for (const heading of linkedHeadings) {
     console.log(heading)
   }
-
 }
 /**
- * Debugging function - lists all the links in the generate ToC, to see if
- * they match headings in the DOM.
+ *
+ * Enrich our table of content with nicer scrolling behaviour
  *
  */
-function listTocLinks() {
+function addScrollingToToC() {
   const tocLinks = document.querySelectorAll('nav .toc a')
 
   for (const link of tocLinks) {
-    console.log(link)
-    console.log(link.hash)
     addScrollBehaviourToLink(link)
   }
 }
 
-
-console.log("LOADED")
+window.addEventListener('DOMContentLoaded', function (event) {
+  addScrollingToToC()
+});
 
