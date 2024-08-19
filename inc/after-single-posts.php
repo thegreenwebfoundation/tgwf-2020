0a22283aa67ba8aabfe45742101abd67d9d94327
hanopcan <hannah@opcan.co.uk>
2024-08-19 16:36:51 +0100

    Improve styling of mailing list signup

--- inc/after-single-posts.php
+++ inc/after-single-posts.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Customisations for single new posts
+ * Customisations to the end of single post content.
  *
  * @package tgwf
  */
@@ -21,7 +21,7 @@ function gwf_after_post_main_content( $content ) {
 
         // Only do this for posts in the CO2.js category.
         if ( in_category( 'CO2.js' ) ) :
-            $content .= '<h3>Get notified about new CO2.js posts</h3>';
+            $content .= '<hr><h4>Get notified about new CO2.js posts</h4>';
             $content .= do_shortcode( "[sibwp_form id=2]" );
         endif;
 
@@ -37,13 +37,13 @@ function output_post_tags() {
     if ( ! is_array( $tags ) ) {
         return;
     }
-    $html  = '<p class="nv-tags-list">';
-    $html .= '<span>' . __( 'Tags', 'neve' ) . ':</span>';
+    $html  = '<div class="nv-tags-list" style="margin-top: 2rem; margin-bottom: 2rem;">';
+    $html .= '<span style="font-weight: bold">' . __( 'Tags', 'neve' ) . ':</span>';
     foreach ( $tags as $tag ) {
         $tag_link = get_tag_link( $tag->term_id );
         $html    .= '<a href=' . esc_url( $tag_link ) . ' title="' . esc_attr( $tag->name ) . '" class=' . esc_attr( $tag->slug ) . ' rel="tag">';
         $html    .= esc_html( $tag->name ) . '</a>';
     }
-    $html .= ' </p> ';
+    $html .= ' </div> ';
     return $html; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
 }
