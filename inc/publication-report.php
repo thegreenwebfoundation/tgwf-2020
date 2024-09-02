827750d03874dcd2adac0a1b638dc9c560459387
hanopcan <hannah@opcan.co.uk>
2024-09-02 18:39:39 +0100

    Make foe generic to reports and include AI briefing report

--- inc/publication-report.php
+++ inc/publication-report.php
@@ -14,8 +14,8 @@ add_action( 'neve_do_sidebar', 'fog_of_enactment_toc', 50, 2 );
 
 /* Adds a container level class for the fog of enactment page. */
 function fog_of_enactment_body_class( $classes ) {
-	if ( is_page( 'report-fog-of-enactment' ) ) {
-		$classes[] = 'fog-of-enactment__page';
+	if ( is_page( 'report-fog-of-enactment' ) || is_page( 'report-ai-environmental-impact' ) ) {
+		$classes[] = 'gwf-publications__report';
 	}
 
     return $classes;
@@ -25,8 +25,11 @@ function fog_of_enactment_body_class( $classes ) {
 /* Adds a container level class for the fog of enactment page. */
 function fog_of_enactment_class( $string ) {
 	if ( is_page( 'report-fog-of-enactment' ) ) {
-    	$string .= ' fog-of-enactment';
+    	$string .= ' gwf-publication-report fog-of-enactment';
+	} elseif ( is_page( 'report-ai-environmental-impact' ) ) {
+		$string .= ' gwf-publication-report ai-env-impact';
 	}
+
 
     return $string;
 }
@@ -34,6 +37,9 @@ function fog_of_enactment_class( $string ) {
 function fog_of_enactment_toc( $context, $position ) {
 
 	if ( is_page( 'report-fog-of-enactment' ) && $position == 'left' ) {
+
+		get_template_part( 'template-parts/fog-of-enactment', '', '');
+	} else if ( is_page( 'report-ai-environmental-impact' ) && $position == 'left' ) {
 
 		get_template_part( 'template-parts/fog-of-enactment', '', '');
 	}
